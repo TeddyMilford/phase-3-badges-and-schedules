8f37c460a3f417389094f90ec7076ac91c1ad65b
Teddy Milford <teddy.milford@gmail.com>
2022-06-26 16:31:36 -0600

    commit tree 011d62df9f3a170557f5287b53940c5cafafd9af

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  arr.map { |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms(arr)
+  arr
+    .map
+    .with_index(1) do |name, index|
+      "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each { |badge| puts badge }
+
+  assign_rooms(attendees).each { |assignment| puts assignment }
+end
